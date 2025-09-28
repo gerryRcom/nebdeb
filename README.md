01c1e849fdc7494c0283d0eeef63f422ba33a083
gerryRcom <gerryr@gerryr.com>
2025-09-28 22:45:14 +0100

    initial service and config build

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# nebdeb
-A means to manage nebula
+## nebdeb
+
+A means to manage nebula, specifically:
+
+- Building of nebula configurations for hosts.
+- Detect when a new host is added and just build a config for that.
+- Allow re-building if a new nebula binary is released.
+- Build a .deb package for ease of installation/ distribution of config.
+
+
+#### TODO
+
+- Allow tayloring of services, my initial requirement is only fo ssh
+- Allow multiple LightHouses, my initial requirement only has one LightHouse
+- Allow simple method to rebuild everything e.g. in the case a cert is exposed
--- nebdeb.py
+++ nebdeb.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 import subprocess
 import hashlib
+import shutil
+import os
 
 
 # Set some constants
 SYSTEMS = "input/systems.csv"
-BINLOCATION = "input/"
+INPUT = "input/"
+OUTPUT = "output/"
 BINHASH = "input/binhash"
 
 
 
-def generateCert(nebulaIP, fileName):
-    certCommand=BINLOCATION+"nebula-cert sign -name "+fileName+" -ip "+nebulaIP
+def generateCert(hostName, nebIP):
+    certCommand=INPUT+"nebula-cert sign -name "+hostName+" -ip "+nebIP
     print(certCommand)
     print(subprocess.call(certCommand, shell=True))
 
 # Get the hash of the nebula binary to comapre against previous run
 def getHash():
-    with open(BINLOCATION+'nebula', 'rb') as nebulaBinary:
+    with open(INPUT+'nebula', 'rb') as nebulaBinary:
         nebulaBinaryContent = nebulaBinary.read()
         return hashlib.sha256(nebulaBinaryContent).hexdigest()
 
@@ -34,8 +37,44 @@ def compareHash():
                 nebulaHash.write(getHash())
                 nebulaHash.close()
                 return False
-        
+
+def buildConfig(hostName,nebIP,amLighthouse,lightHouse):
+    # Define placeholder entries
+    PH_HOST="##HOSTNAME##"
+    PH_IP="##HOSTIP##"
+    PH_AMLIGHTHOUSE="##AMLIGHTHOUSE##"
+    PH_LIGHTHOUSE="##LIGHTHOUSE##"
+
+    os.makedirs(OUTPUT+hostName,exist_ok=True)
+    shutil.copyfile(INPUT+'nebula.yml', OUTPUT+hostName+'/'+hostName+'.yml')
+
+    # read in the nebula template, substitute placeholders and output host's nebula config file
+    with open(OUTPUT+hostName+'/'+hostName+'.yml', 'rt') as configFile:
+        configData = configFile.read()
+        configData = configData.replace(PH_HOST, hostName)
+        configData = configData.replace(PH_IP, nebIP)
+        configData = configData.replace(PH_LIGHTHOUSE, lightHouse)
+        configData = configData.replace(PH_AMLIGHTHOUSE, amLighthouse)
+        configFile.close()
+    with open(OUTPUT+hostName+'/'+hostName+'.yml', 'wt') as configFile:
+        configFile.write(configData)
+        configFile.close()
+
+def buildService(hostName):
+    # Define placeholder entries
+    PH_HOST="##HOSTNAME##"
+    shutil.copyfile(INPUT+'nebula.service', OUTPUT+hostName+'/nebula.service')
+
+    # read in the service template, substitute placeholders and output host's service file
+    with open(OUTPUT+hostName+'/nebula.service', 'rt') as serviceFile:
+        serviceData = serviceFile.read()
+        serviceData = serviceData.replace(PH_HOST, hostName)
+        serviceFile.close()
+    with open(OUTPUT+hostName+'/nebula.service', 'wt') as serviceFile:
+        serviceFile.write(serviceData)
+        serviceFile.close()
 
 
 if __name__ == "__main__":
-    compareHash()
+    buildConfig("testbane","199.222.222.222","false","1.1.1.1")
+    buildService("testbane")
